90ba70186ba20268e58d2491c46cbac4a7a48e59
Gabriel Fernandes <gabgui2001@gmail.com>
2022-06-27 23:46:32 -0300

    .

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <div className="bg-red-100 w-3/6 mx-auto h-full">
+        <h1 className="text-2xl p-4 mx-auto">oi</h1>
+    </div>
+  )
+}
+
+export default App
